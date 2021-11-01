9b8c3289dc38e46cdd96827653fa54b771141ff0
Alexandru Lupei <lupei.alexandru2001@gmail.com>
2021-11-01 19:54:24 +0200

    meniu nou

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from Logic.CRUD import adaugareVanzare
 from Tests.testALL import runalltests
 from UI.console import runMenu
+from UI.newconsole import runnewMenu
+
 
 def main():
     runalltests()
     lista = []
     lista = adaugareVanzare("1", "Moarte pe nil", "politist", 100, "gold", lista)
     lista = adaugareVanzare("2", "Criminalul ABC", "politist", 1000, "silver", lista)
-    runMenu(lista)
+    runnewMenu(lista)
 
 main()
